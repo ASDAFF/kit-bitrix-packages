673fff030dcfde5f3193fc41d0efb0180a7b4edb
ASDAFF <asdaff.asad@yandex.ru>
2020-08-29 20:55:50 +0500

    up

--- new-packages/sotbit.origami/install/wizards/sotbit/origami/site/templates/.default/components/bitrix/menu/origami_main_header_sidebar/lang/ru/template.php
+++
@@ -1,5 +0,0 @@
-<?
-$MESS['MENU_ITEM_ACCESS_DENIED'] = "Доступ запрещен";
-$MESS['SOTBIT_MAIN_MENU_MORE'] = "Ещё";
-$MESS['SOTBIT_MAIN_MENU_CATALOG'] = "Каталог";
-?>
--- new-packages/sotbit.origami/install/wizards/sotbit/origami/site/templates/.default/components/bitrix/news/sotbit_origami_blog/bitrix/news.list/popular/lang/ru/template.php
+++
@@ -1,5 +0,0 @@
-<?
-$MESS["CT_BNL_ELEMENT_DELETE_CONFIRM"] = "Будет удалена вся информация, связанная с этой записью. Продолжить?";
-$MESS["NEWS_LIST_READ"] = "Читать статью";
-$MESS["NEWS_LIST_TITLE"] = "Интересные статьи";
-?>
