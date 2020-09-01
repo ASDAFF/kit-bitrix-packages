652bebb50c90c4ac8a59ec031ee86c87f2a9e04b
ASDAFF <asdaff.asad@yandex.ru>
2020-09-01 13:03:24 +0500

    up

--- new-packages/sotbit.origami/install/wizards/sotbit/origami/site/templates/.default/components/bitrix/news/sotbit_origami_blog/bitrix/news.detail/origami_blog/component_epilog.php
+++
@@ -1,8 +0,0 @@
-<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
-
-use Bitrix\Main\Page\Asset;
-Asset::getInstance()->addJs(SITE_DIR . "local/templates/sotbit_origami/assets/plugin/swiper5.2.0/js/swiper.js");
-Asset::getInstance()->addJs(SITE_DIR . "local/templates/sotbit_origami/assets/js/custom-slider.js");
-Asset::getInstance()->addCss(SITE_DIR . "local/templates/sotbit_origami/assets/plugin/swiper5.2.0/css/swiper.min.css");
-Asset::getInstance()->addCss(SITE_DIR . "local/templates/sotbit_origami/assets/css/style-swiper-custom.css");
-?>
